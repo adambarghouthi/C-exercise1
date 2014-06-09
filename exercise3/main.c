d3afe8b8391c1b8c146bdf0773f4d387f4e1d051
Adam Albarghouthi <adambargh@gmail.com>
2014-06-09 13:19:59 -0400

    Initial Commit

---
+++ exercise3/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  exercise3
+//
+//  Created by Adam Albarghouthi on 2014-06-09.
+//  Copyright (c) 2014 Adam Albarghouthi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
